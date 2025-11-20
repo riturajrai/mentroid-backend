36df159a3b27db043c771b937afbb555c9aa2a21
Rituraj Rai <162144998+riturajrai@users.noreply.github.com>
2025-11-20 19:49:49 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -15,8 +15,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://mentoroid-frontend.up.railway.app",
-      "https://mentoroid-frontend-production.up.railway.app"
+      "https://mentoroid-frontend.vercel.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
@@ -43,3 +42,4 @@ const startServer = async () => {
 };
 
 startServer();
+
